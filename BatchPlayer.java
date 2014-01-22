ac524e388ec4eff73d3d92c525c2f5f91b1d0923
Frederick Widjaja <frederick_widjaja@hotmail.com>
2014-01-22 11:46:13 -0500

    Updated BatchPlayer

--- BatchPlayer.java
+++ BatchPlayer.java
@@ -8,12 +8,13 @@ public class BatchPlayer {
         int numTrials = Integer.parseInt(args[0]);
         String[] cmd = Arrays.copyOfRange(args, 1, args.length);
 
-        System.out.println("Running process " + cmd[1]);
+        System.out.println("Running process \"" + join(cmd) + "\" for " + (numTrials == 0 ? "infinite" : numTrials) + " iterations");
+        System.out.println("No.\tResult\tWins\t\tLosses\t\t% Wins");
         int numWins = 0;
         int numLosses = 0;
-        for (int i = 1; i <= numTrials; i++) {
+        for (int i = 1; numTrials == 0 || i <= numTrials; i++) {
             try {
-                System.out.print("Trial " + i + "; Result: ");
+                System.out.print(i + "\t");
                 boolean ended = false;
 
                 ProcessBuilder pb = new ProcessBuilder(cmd);
@@ -39,9 +40,9 @@ public class BatchPlayer {
                     System.out.print("Error");
                 }
 
-                System.out.print("; Wins: " + numWins);
-                System.out.print("; Losses: " + numLosses);
-                System.out.println("; % Wins: " + (numWins * 100.0 / (numWins + numLosses)) + "%");
+                System.out.print("\t" + numWins);
+                System.out.print("\t\t" + numLosses);
+                System.out.println("\t\t" + (numWins * 100.0 / (numWins + numLosses)) + "%");
 
                 p.destroy();
             } catch (Exception e) {
@@ -53,6 +54,17 @@ public class BatchPlayer {
         System.out.println("Wins: " + numWins);
         System.out.println("Losses: " + numLosses);
         System.out.println("% Wins: " + (numWins * 100.0 / (numWins + numLosses)) + "%");
+    }
+
+    private static String join(String[] s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length; i++) {
+            if (i > 0) {
+                sb.append(" ");
+            }
+            sb.append(s[i]);
+        }
+        return sb.toString();
     }
 
 }
